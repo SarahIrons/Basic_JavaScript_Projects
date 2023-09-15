77c20ee303f73b0214ab530b79de299907767cb2
SarahIrons <sarahirons11@gmail.com>
2023-09-15 13:23:51 -0400

    Pushing Javascript assignments to git folder repo

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,43 @@
+var A ="I have two cats."
+
+document.write(A);
+// this displays the variable A text.
+
+document.write("<br>");
+// this is a way to put breaks in the displayed text for increased readability. (used throughout the document)
+
+var B="This is an alert, Hello!";
+window.alert(B);
+// this is a way to run/call the info for the alert text box that appears upon opening the page.
+
+document.write("Victor Hugo said,\"Forty is the old age of youth; fifty the youth of old age.\"")
+// this is an example using quotes within JS syntax/coding.
+
+document.write("<br>")
+
+document.write("\"We were at a party, His earlobe fell in the deep,  \Someone reached in and grabbed it, It was a rock lobster.\"")
+// this is a concatenated and quoted text.
+
+document.write("<br>")
+
+var C="Twistin\' round the fire, Havin\' fun&nbsp" + "Bakin\' potatoes bakin\' in the sun";
+document.write(C)
+// this text had examples of apostrophes enclosed in the displayed text, concatenated text, and also a built-in space in the text for increased readability.
+
+document.write("<br>")
+
+var Family = "The Iron Roaches", PersonA='Pat', PersonB='Sarah', Cat1='Carlo', Cat2='Jody';
+document.write(Cat2);
+// this will display the text 'Jody' since he is the variable component cat2. 
+
+document.write("<br>")
+
+document.write(4*3);
+// this is a multiplication expression.
+document.write("<br>")
+
+var sent1="This is the beginning of the string&nbsp";
+var sent2="and this is the end of the string.";
+document.write(sent1+sent2)
+
+// this returns/displays the text as concatenated as one full phrase and I also included the 'nbsp' phrase at end of sent1 to create a space in the displayed/returned text. 
